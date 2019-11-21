71c1e04a9e6ca2b3eb3c52e86176e7a7ddcd082f
Your Name <you@example.com>
2019-11-21 20:11:39 +0200

    Navbar

--- ques-page/src/App.js
+++ ques-page/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from './componets/Navbar';
 import Footer from "./componets/Footer";
+
 
 class App extends Component {
   state = {};
   render() {
     return (
       <div className="app">
+        <Navbar />
         <Footer />
       </div>
     );
